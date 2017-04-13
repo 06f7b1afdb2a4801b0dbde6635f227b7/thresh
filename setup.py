1c23395177cf0751d4c47ceab91e0c1d37f87626
sswan <scot.swan@gmail.com>
2017-04-13 10:32:48 +0200

    Pulled everything into one file, no longer a module.

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 """
 This blank setup file is placed here to make pytest put the .cache directory
-here instead of in the tests directory. Actual content will be added later
-when the module is more mature.
+here instead of in the tests directory.
 """
--- thresh/__main__.py
+++
@@ -1,6 +0,0 @@
-""" A small wrapper around the main() function. """
-import sys
-from .__init__ import main
-
-if __name__ == '__main__':
-    main(sys.argv[1:])
